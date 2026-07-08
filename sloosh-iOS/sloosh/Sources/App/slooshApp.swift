4373f25d00c11deb6d82a893569e8aac887abaf1
Администратор <okroshka.p@gmail.com>
2026-07-08 16:38:07 +0500

    fix: restore portrait orientation on player dismiss

--- sloosh-iOS/sloosh/Sources/App/slooshApp.swift
+++ sloosh-iOS/sloosh/Sources/App/slooshApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import AVFoundation
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    static var orientationLock = UIInterfaceOrientationMask.all
+    static var orientationLock = UIInterfaceOrientationMask.portrait
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return AppDelegate.orientationLock
