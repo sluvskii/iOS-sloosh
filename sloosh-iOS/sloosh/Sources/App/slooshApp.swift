c83b592f6a3c2e9f68011cf4e4b32233166f3f51
Администратор <okroshka.p@gmail.com>
2026-07-15 02:26:51 +0500

    fix(navigation): re-enable swipe-to-go-back gesture globally when navigation bar is hidden

--- sloosh-iOS/sloosh/Sources/App/slooshApp.swift
+++ sloosh-iOS/sloosh/Sources/App/slooshApp.swift
@@ -96,3 +96,14 @@ struct slooshApp: App {
         }
     }
 }
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
